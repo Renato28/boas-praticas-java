cbe016a33b94280c0cc52abbee5f77ed27d9c1da
Renato Nobrega <renatonobrega87@gmail.com>
2026-05-24 21:49:24 -0300

    Implementando a entidade Abrigo

---
+++ src/main/java/br/com/github/renato28/domain/Abrigo.java
@@ -0,0 +1,71 @@
+package br.com.github.renato28.domain;
+
+public class Abrigo {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Pet[] pets;
+
+    public Abrigo() {
+
+    }
+
+    public Abrigo(String nome, String telefone, String email, Pet[] pets) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.pets = pets;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public void setPets(Pet[] pets) {
+        this.pets = pets;
+    }
+
+    @Override
+    public String toString() {
+        return "Abrigo{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
